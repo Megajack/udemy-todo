ee5d9d89b7e6ef3cbf5d6ba38270bbd3c7e84eb6
Eugene Leleka <eleleka.skilld.fr>
2020-05-13 00:43:13 +0300

    work with arrays

--- src/index.js
+++ src/index.js
@@ -5,11 +5,17 @@ import TodoList from './components/todo-list';
 import SearchPanel from './components/search-panel';
 
 const App = () => {
+  const todoData = [
+    { label: 'drink coffee', important: false },
+    { label: 'make awesome app', important: true },
+    { label: 'have a lunch', important: false }
+  ];
+
   return (
     <div>
-      <AppHeader/>
-      <SearchPanel/>
-      <TodoList/>
+      <AppHeader />
+      <SearchPanel />
+      <TodoList todos={todoData} />
     </div>
   );
 }
