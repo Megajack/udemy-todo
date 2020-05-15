43b34c742d69b92993ad4ace78fcaf9e4c90d505
Eugene Leleka <eleleka.skilld.fr>
2020-05-16 00:30:52 +0300

    add click behavior and state for item-list-item

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -2,8 +2,24 @@ import React, { Component } from 'react';
 import './todo-list-item.css';
 
 export default class TodoListItem extends Component {
+  state = {
+    done: false
+  }
+
+  onLabelClick= () => {
+    this.setState({
+      done: true
+    });
+  }
+
   render() {
     const { label, important = false } = this.props;
+    const { done } = this.state;
+
+    let classNames = 'todo-list-item';
+    if (done) {
+      classNames += ' done';
+    }
 
     const style = {
       color: important ? 'steelblue' : 'black',
@@ -11,8 +27,8 @@ export default class TodoListItem extends Component {
     };
 
     return (
-      <span className="todo-list-item">
-        <span className="todo-list-item-label" style={ style }>{ label }</span>
+      <span className={ classNames }>
+        <span className="todo-list-item-label" onClick={ this.onLabelClick } style={ style }>{ label }</span>
         <button type="button" className="btn btn-outline-success btn-sm">
           !
         </button>
